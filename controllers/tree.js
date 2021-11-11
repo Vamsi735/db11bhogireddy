dc8aeaa1d4168bcb4ddf116cf501f5d92868cc36
vamsi <vamsibhogireddy735@gmail.com>
2021-11-11 12:33:31 -0600

    modified files

--- controllers/tree.js
+++ controllers/tree.js
@@ -26,9 +26,25 @@ exports.tree_view_all_Page = async function(req, res) {
 }; 
  
 // Handle tree create on POST. 
-exports.tree_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: tree create POST'); 
-}; 
+exports.tree_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new tree(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"tree_type":"goat", "cost":12, "size":"large"} 
+    document.tree_type = req.body.tree_type; 
+    document.size = req.body.size; 
+    document.age = req.body.age; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+};  
  
 // Handle tree delete form on DELETE. 
 exports.tree_delete = function(req, res) { 
