e68c99da849c65cfa47b04af9433cb3ea2c4cc74
vamsi <vamsibhogireddy735@gmail.com>
2021-11-28 15:48:32 -0600

    Bonus screen shot

--- models/tree.js
+++ models/tree.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 const treeSchema = mongoose.Schema({
-    tree_type: String,
-    size: String,
+    tree_type: {
+        type: String,
+        minlength:4
+    },
+    size: {
+        type: String,
+        minlength:7
+    },
     age: String
 })
 module.exports = mongoose.model("tree", treeSchema)
--- routes/tree.js
+++ routes/tree.js
@@ -1,6 +1,16 @@
 var express = require('express'); 
 const tree_controlers= require('../controllers/tree'); 
 var router = express.Router(); 
+// A little function to check if we have an authorized user and continue on 
+or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET tree */ 
 router.get('/', tree_controlers.tree_view_all_Page ); 
@@ -15,13 +25,13 @@ router.get("/", function (req, res, next) {
 router.get('/detail', tree_controlers.tree_view_one_Page);
 
 /* GET create tree page */ 
-router.get('/create', tree_controlers.tree_create_Page); 
+router.get('/create',secured, tree_controlers.tree_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', tree_controlers.tree_update_Page); 
+router.get('/update',secured, tree_controlers.tree_update_Page); 
 
 /* GET create tree page */ 
-router.get('/delete', tree_controlers.tree_delete_Page); 
+router.get('/delete',secured, tree_controlers.tree_delete_Page); 
  
 
 module.exports = router;
